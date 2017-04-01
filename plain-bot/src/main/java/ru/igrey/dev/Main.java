b21173f6278e192249bfe1411968dc3b7bd55843
sanasov <igrey@mail.ru>
2017-04-01 15:04:06 +0300

    create project

---
+++ plain-bot/src/main/java/ru/igrey/dev/Main.java
@@ -0,0 +1,10 @@
+package ru.igrey.dev;
+
+/**
+ * Created by sanasov on 01.04.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
